fd8ff6dd645eb23d24dba4dc923e4cf6fb0d4b0a
Isabella Hidalgo <isabellalaurenhidalgo@gmail.com>
2022-07-12 12:39:18 -0700

    Adding R Script

---
+++ loadingData.R
@@ -0,0 +1,11 @@
+#### load in csv file ####
+#now this data is an R object, storing the contents of the csv file
+cerealData <- read.csv("data/cerealData.csv")
+View(cerealData)
+
+#### loading in ggplot ####
+install.packages("ggplot2")
+library(ggplot2)
+
+#### practice ####
+#plotting with your dataset
