c74d64a0ec330e1cda01c10f7022c18379e6c84b
Isabella Hidalgo <isabellalaurenhidalgo@gmail.com>
2022-07-14 10:44:28 -0700

    Updates on initial data

--- loadingData.R
+++ loadingData.R
@@ -2,6 +2,8 @@
 #now this data is an R object, storing the contents of the csv file
 cerealData <- read.csv("data/cerealData.csv")
 View(cerealData)
+#saveRDS(cerealData, "data/cerealData.RDS")
+#saving R object as a file (if the file is too large) ^^
 
 #### loading in ggplot ####
 install.packages("ggplot2")
@@ -9,3 +11,8 @@ library(ggplot2)
 
 #### practice ####
 #plotting with your dataset
+ggplot(data = cerealData, aes(x = calories_content, y = fat_content))  +
+  geom_point()
+
+ggplot(data = cerealData, aes(x = Producer)) +
+  geom_bar()
