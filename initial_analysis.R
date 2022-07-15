5e5048da9ad7874bda9a84517f89a1a45d247069
Isabella Hidalgo <isabellalaurenhidalgo@gmail.com>
2022-07-15 12:01:18 -0700

    Adding data analysis and graphs.

--- initial_analysis.R
+++ initial_analysis.R
@@ -17,4 +17,24 @@ cerealData %>%
   geom_point()
 
 
+cerealData %>% 
+  filter(Producer == "K") %>%
+  ggplot(aes(x = calories_content, y = Producer)) +
+  geom_violin(color = "pink", fill = "pink") + 
+  labs(title = "Quantity of Calories in K Producer")
+
+cerealData%>%
+  sample_n(10) %>%
+  arrange(grain_name) %>%
+  ggplot(aes(x = calories_content, y = fat_content)) +
+  geom_point(color = "purple") 
+  #to change the scale of a graph,
+  #scale_x_continuous(breaks = seq(min, max, step))
+
+cerealData_small <- filter(cerealData, Producer == "K")
+cerealData_small%>%
+  summarize(mean_cals = mean(calories_content)) 
+
+#cerealData%>%
+  #mutate()
 
