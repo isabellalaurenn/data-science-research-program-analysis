bb4061133687cc21ec51e9d96cfdeea7dea40297
Isabella Hidalgo <isabellalaurenhidalgo@gmail.com>
2022-07-20 11:38:22 -0700

    Machine Learning notes and practice.

--- machine_learning.R
+++ machine_learning.R
@@ -24,9 +24,9 @@ ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) +
   geom_point(aes(color = Species))
 
 
-cor(iris$Petal.Length, iris$Petal.Length)
-cor(iris$Petal.Length, iris$Sepal.Width)
-cor(iris$Petal.Length, iris$Petal.Width)
+cor(iris$Petal.Length, iris$Petal.Length) #1
+cor(iris$Petal.Length, iris$Sepal.Width)  #-.4284401
+cor(iris$Petal.Length, iris$Petal.Width)  #.9628654
 
 #choose features
 #Petal.Width, Sepal.Width, Sepal.Length
@@ -57,12 +57,110 @@ iris_valid <- filter(iris, label == "validation")
 
 #linear model
 
-iris_lm <- lm(Petal.Length ~ Petal.Width + Sepal.Length + Sepal.Width, data = iris_train)
+iris_lm <- lm(Petal.Length ~ Petal.Length + Sepal.Length + Sepal.Width, data = iris_train)
+#smallest p value first
 iris_lm
+summary(iris_lm) #if we need to rearrange stuff in the pluses
+#petal width is like x1, sepal length is like x2, sepal width is like x3
 
 #select out only the x values we use (petal width and sepal length)
 iris_lm_predictions <- select(iris_test, Petal.Width, Sepal.Length, Sepal.Width) %>%
-  predict(object = iris_lm) #object is the model that we just created
+  predict(object = iris_lm, type = "response") #object is the model that we just created
 
-iris_train$lm_pred <- iris_lm_predictions
-head(iris_train)
+iris_test$lm_pred <- iris_lm_predictions
+head(iris_test)
+
+
+#logistic model
+
+#create our two categories
+#mean(iris$Petal.Length) = 3.758
+iris_train_glm <- iris_train %>%
+  mutate(petal_length_cat = as.factor(ifelse(Petal.Length < 3.758, "short", "long")))
+head(iris_train_glm)
+
+iris_glm <- glm(petal_length_cat ~ Sepal.Length + Sepal.Width + Species, 
+                data = iris_train_glm,
+                family = binomial(link = "logit"))
+#warning is okay, errors are not lol
+
+iris_glm_preds <- iris_test %>%
+  select(Sepal.Length, Sepal.Width,  Species) %>%
+  predict(object = iris_glm) #object means which model are we using, and only
+# give the x models that we need
+
+
+iris_test$glm_pred <- iris_glm_preds
+
+iris_test <- iris_test %>%
+  mutate(petal_length_cat = as.factor(ifelse(Petal.Length < 3.758, "short", "long")))
+head(iris_test)
+
+#filter down to 2 categories
+iris_train_2species <- filter(iris_train, Species %in% c("setosa", "virginica"))
+
+#create the model
+iris_glm <- glm(Species ~ Sepal.Width + Sepal.Length + Petal.Width + Petal.Length, 
+                data = iris_train_2species, 
+                family = binomial(link = "logit"))
+
+#create test set with only 2 species
+iris_test_2species <- iris_test %>% 
+  filter(Species %in% c("setosa", "virginica"))
+#make predictions based on model
+iris_2species_preds <- iris_test_2species %>%
+  select(-Species) %>%
+  predict(object = iris_glm)
+#add predictions to test set
+iris_test_2species$glm_2spec_pred <- iris_2species_preds
+
+
+#generalized boosted (making own corrections) regression modeling
+install.packages("gbm")
+library(gbm)
+
+#create model
+iris_gbm <- gbm(Petal.Length ~ Petal.Width + Sepal.Length + Sepal.Width + Species, 
+                data = iris,
+                n.trees = 500)#iris_test was too small, make sure to use test for your data
+#order doesnt matter as much for this, can be rlly anything
+summary(iris_gbm) #gives a plot! woah, see the relative influence on the petal length
+
+#select out only x vals from test and predict
+iris_gbm_preds <- iris_test %>%
+  select(Petal.Width, Sepal.Length, Sepal.Width, Species) %>%
+  predict(object = iris_gbm)
+
+#save predictions back into test set
+iris_test$gbm_pred <- iris_gbm_preds
+
+#evaluate performance of models
+View(iris_test)
+
+
+#error
+install.packages("Metrics") #was DMwR but its old lol
+library(Metrics)
+
+#calculate rmse between predictions and true evalues
+rmse(iris_test$Petal.Length, iris_test$lm_pred)
+rmse(iris_test$Petal.Length, iris_test$gbm_pred) #wins! smaller error
+
+#calculate mae between predictions and true values
+mae(iris_test$Petal.Length, iris_test$lm_pred) 
+mae(iris_test$Petal.Length, iris_test$gbm_pred) #wins! smaller error
+
+
+#accuracy
+iris_test <- iris_test %>%
+  mutate(glm_petal_cat = ifelse(glm_pred < 0, "long", "short"))
+View(iris_test)
+
+true_vals <- sum(iris_test$glm_petal_cat == iris_test$petal_length_cat)
+total_vals <- nrow(iris_test)
+
+accuracy <- true_vals/total_vals
+
+#f1 score tells us about false positive and false negative rates
+f1(iris_test$glm_petal_cat, iris_test$petal_length_cat)
+
